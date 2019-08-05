6e6f996089c482744578add6aa2fe939e4a4759f
RomanMy <romanmykhailetskyi@gmail.com>
2019-08-05 23:23:38 +0300

    RightLogInPart and RightLogOutPart updated

--- OpenCart414Test/OpenCart414Test/Pages/RightLogInPart.cs
+++ OpenCart414Test/OpenCart414Test/Pages/RightLogInPart.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,40 @@ using System.Threading.Tasks;
 
 namespace OpenCart414Test.Pages
 {
-    class RightLogInPart
+    class RightLogInPart : RightMenuPart
     {
+        public IWebElement RightMenuEditAccount
+        { get { return driver.FindElement(By.XPath("//div[@class='list-group']/a[text()='Edit Account']")); } }
+        public IWebElement RightMenuPassword
+        { get { return driver.FindElement(By.XPath("//div[@class='list-group']/a[text()='Password']")); } }
+        public IWebElement RightMenuLogout
+        { get { return driver.FindElement(By.XPath("//div[@class='list-group']/a[text()='Logout']")); } }
+        public RightLogInPart(IWebDriver driver) : base(driver)
+        {
+        }
+        public string GetRightMenuEditAccountText()
+        {
+            return RightMenuEditAccount.Text;
+        }
+        public void ClickRightMenuEditAccount()
+        {
+            RightMenuEditAccount.Click();
+        }
+        public string GetRightMenuPasswordText()
+        {
+            return RightMenuPassword.Text;
+        }
+        public void ClickRightMenuPassword()
+        {
+            RightMenuPassword.Click();
+        }
+        public string GetRightMenuLogoutText()
+        {
+            return RightMenuLogout.Text;
+        }
+        public void ClickRightMenuLogout()
+        {
+            RightMenuLogout.Click();
+        }
     }
 }
--- OpenCart414Test/OpenCart414Test/Pages/RightLogOutPart.cs
+++ OpenCart414Test/OpenCart414Test/Pages/RightLogOutPart.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace OpenCart414Test.Pages
 {
-    class RightLogOutPart
+    class RightLogOutPart : RightMenuPart
     {
+        public IWebElement RightMenuLogin
+        { get { return driver.FindElement(By.XPath("//div[@class='list-group']/a[text()='Login']")); } }
+        public IWebElement RightMenuRegister
+        { get { return driver.FindElement(By.XPath("//div[@class='list-group']/a[text()='Register']")); } }
+        public RightLogOutPart(IWebDriver driver) : base(driver)
+        {
+        }
+        public string GetRightMenuLoginText()
+        {
+            return RightMenuLogin.Text;
+        }
+        public void ClickRightMenuLogin()
+        {
+            RightMenuLogin.Click();
+        }
+        public string GetRightMenuRegisterText()
+        {
+            return RightMenuRegister.Text;
+        }
+        public void ClickRightMenuRegister()
+        {
+            RightMenuRegister.Click();
+        }
     }
 }
