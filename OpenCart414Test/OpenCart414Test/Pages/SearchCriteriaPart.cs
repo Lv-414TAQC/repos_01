078474bb32a162069e83a6736c5b36676663a3c6
Nazarii Yurkiv <nazar-y96@ukr.net>
2019-08-06 14:55:51 +0300

    Test commit

--- OpenCart414Test/OpenCart414Test/Pages/SearchCriteriaPart.cs
+++ OpenCart414Test/OpenCart414Test/Pages/SearchCriteriaPart.cs
@@ -33,7 +33,7 @@ namespace OpenCart414Test.Pages
             IWebElement temp = CriteriaSearchField; // TODO All Web Elements
         }
 
-        // Page Object
+        // Page Object f5
 
         // CriteriaSearchField
         public string GetCriteriaSearchFieldText()
