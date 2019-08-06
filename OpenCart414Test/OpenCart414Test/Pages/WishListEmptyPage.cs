9b8ebb387eb3aa92619a4da64213086ec287552b
RomanMy <romanmykhailetskyi@gmail.com>
2019-08-06 18:47:11 +0300

    WishListComponent and WishListComponentContainer created, WishListEmptyPage and WishListPage updated

--- OpenCart414Test/OpenCart414Test/Pages/WishListEmptyPage.cs
+++ OpenCart414Test/OpenCart414Test/Pages/WishListEmptyPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace OpenCart414Test.Pages
 {
-    class WishListEmptyPage
+    class WishListEmptyPage : WishListPart
     {
+        public IWebElement WishListIsEmptyParagraph
+        { get { return driver.FindElement(By.XPath("//div[@id='content']/p")); } }
+        public WishListEmptyPage(IWebDriver driver) : base(driver)
+        {
+        }
+        public string GetWishListIsEmpltyParagraphText()
+        {
+            return WishListIsEmptyParagraph.Text;
+        }
     }
 }
