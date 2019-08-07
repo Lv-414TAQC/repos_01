e309494f77d6846b54d850f194620c3edae74dd2
OlegStetsula <olegstetsula@gmail.com>
2019-08-07 10:18:54 +0300

    AddTaxRatePage implementation added

--- OpenCartTests/Stetsula/Pages/TaxRatesPage.cs
+++ OpenCartTests/Stetsula/Pages/TaxRatesPage.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace OpenCartTests.Stetsula.Pages
 {
-    class TaxRatesPage
+    class TaxRatesPage : HeaderPart
     {
+        IWebElement AddNewButton { get { return Driver.FindElement(By.CssSelector("a[data-original-title='Add New']")); } } 
+        IWebElement DeleteButton { get { return Driver.FindElement(By.CssSelector("button[data-original-title='Delete']")); } } 
+        IList<IWebElement> TaxRates = new List<IWebElement>();
+
+        public TaxRatesPage(IWebDriver driver) : base(driver)
+        {
+            
+
+        }
+
+        public void GetTaxrates()
+        {
+
+        }
+
+        public void AddNewTaxRate()
+        {
+
+        }
     }
 }
