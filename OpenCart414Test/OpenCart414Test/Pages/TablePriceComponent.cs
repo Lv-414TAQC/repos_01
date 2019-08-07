16e470906eb15cf81e3de5e3f468d9b5a02209e0
KukliakT <kuklyak.tarass@gmail.com>
2019-08-07 22:54:53 +0300

    Update CartProductContainer and TablePriceContainer

--- OpenCart414Test/OpenCart414Test/Pages/TablePriceComponent.cs
+++ OpenCart414Test/OpenCart414Test/Pages/TablePriceComponent.cs
@@ -9,83 +9,34 @@ namespace OpenCart414Test.Pages
 {
     class TablePriceComponent
     {
-        //private IWebDriver driver;
         protected IWebDriver driver;
-        public TablePriceComponent()
-        {
 
-        }
-        public IWebElement Sub_total
-        { get { return driver.FindElement(By.XPath("//div[@class='row']/div/table/tbody/tr/td/strong[text() ='Sub-Total:']/../following-sibling::td")); } }
-        public IWebElement Sub_total_Name
-        { get { return driver.FindElement(By.XPath("//div[@class='row']/div/table/tbody/tr/td/strong[text() ='Sub-Total:']")); } }
-        public IWebElement Eco_tax
-        { get { return driver.FindElement(By.XPath("//div[@class='row']/div/table/tbody/tr/td/strong[text() ='Eco Tax (-2.00):']/../following-sibling::td")); } }
-        public IWebElement Eco_tax_Name
-        { get { return driver.FindElement(By.XPath("//div[@class='row']/div/table/tbody/tr/td/strong[text() ='Eco Tax (-2.00):']")); } }
-        public IWebElement Vat
-        { get { return driver.FindElement(By.XPath("//div[@class='row']/div/table/tbody/tr/td/strong[text() ='VAT (20%):']/../following-sibling::td")); } }
-        public IWebElement Vat_Name
-        { get { return driver.FindElement(By.XPath("//div[@class='row']/div/table/tbody/tr/td/strong[text() ='VAT (20%):']")); } }
-        public IWebElement total
-        { get { return driver.FindElement(By.XPath("//div[@class='row']/div/table/tbody/tr/td/strong[text() ='Total:']/../following-sibling::td")); } }
-        public IWebElement total_Name
-        { get { return driver.FindElement(By.XPath("//div[@class='row']/div/table/tbody/tr/td/strong[text() ='Total:']")); } }
+        public IList<IWebElement> TablePriceList
+        { get; private set; }
 
-        public TablePriceComponent(IWebDriver driver)
+        public TablePriceComponent(IWebDriver driver, By searchLocator)
         {
             this.driver = driver;
-            CheckElements();
-            InitElements();
+            InitElements(searchLocator);
         }
 
-        private void CheckElements()
+        private void InitElements(By searchLocator)
         {
-            // TODO Develop Custom Exception
-            //IWebElement temp = ViewCartLink;
-            //temp = CheckOutLink; // TODO All Web Elements
+            TablePriceList = driver.FindElements(searchLocator);
         }
 
-        //private void InitElements()
-        //{
-        //    itemsTable = new List<ShoppingCartContainerComponent>();
-        //    foreach (IWebElement current in driver.FindElements(By.CssSelector(ITEMS_TABLE_CSSSELECTOR)))
-        //    {
-        //        itemsTable.Add(new ShoppingCartContainerComponent(current));
-        //    }
-        //}
+
         //Page Object
-        public string GetSub_total_NameText()
+
+
+        public IList<string> GetTablePriceListText()
         {
-            return Sub_total_Name.Text;
-        }
-        public string GetSub_totalText()
-        {
-            return Sub_total.Text;
-        }
-        public string GetEco_Taxl_NameText()
-        {
-            return Eco_tax_Name.Text;
-        }
-        public string GetEco_taxText()
-        {
-            return Eco_tax.Text;
-        }
-        public string GetVat_NameText()
-        {
-            return Vat_Name.Text;
-        }
-        public string GetVatText()
-        {
-            return Vat.Text;
-        }
-        public string GetTotalText()
-        {
-            return total.Text;
-        }
-        public string Get_total_NameText()
-        {
-            return total_Name.Text;
+            IList<string> result = new List<string>();
+            foreach (IWebElement current in TablePriceList)
+            {
+                result.Add(current.Text);
+            }
+            return result;
         }
     }
 }
