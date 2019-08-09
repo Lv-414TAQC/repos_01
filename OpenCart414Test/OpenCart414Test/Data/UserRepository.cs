f19ae2ad6a36c21e2c9b6a6f62eaf0c6f1157fb6
RomanMy <romanmykhailetskyi@gmail.com>
2019-08-09 22:05:36 +0300

    UserRepository fixed

--- OpenCart414Test/OpenCart414Test/Data/UserRepository.cs
+++ OpenCart414Test/OpenCart414Test/Data/UserRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleAppPractices
+namespace OpenCart414Test.Data
 {
     public sealed class UserRepository
     {
