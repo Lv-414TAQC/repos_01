53ca65e4f14a7899709cb0db16172e21d1f6bb74
DimaDomashovets <ddomashovets@gmail.com>
2019-08-24 13:44:18 +0300

    Fixed tests

--- Rest414Test/Rest414Test/RestTest1.cs
+++ Rest414Test/Rest414Test/RestTest1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using RestSharp;
 using RestSharp.Serialization.Json;
@@ -16,94 +15,129 @@ namespace Rest414Test
         }
     }
 
-    //[TestClass]
     [TestFixture]
-    //[Parallelizable(ParallelScope.All)]
     public class RestTest1
     {
-        //[TestMethod]
-        //[Test]
+        [Test]
         public void TestMethod1()
         {
-            // Common
             string url = "http://localhost:8080";
             var client = new RestClient(url);
             JsonDeserializer deserial = new JsonDeserializer();
-            //
-            // Login
-            var request = new RestRequest("/login", Method.POST);
-            request.AddParameter("name", "admin");          // Save parameters to body
-            request.AddParameter("password", "qwerty");
-            //
+            //reset
+            var request = new RestRequest("/reset", Method.GET);
             IRestResponse response = client.Execute(request);
-            //var content = response.Content;
-            //Console.WriteLine("Login content: " + content);
             //
-            // Deserialize
-            var obj = deserial.Deserialize<RestResult>(response);
-            //Console.WriteLine("\nDeserialize content, TOKEN: " + obj);
-            string token = obj.content;
-            //Assert is Logined
-            //
-            // Change Tokenlifetime
-            request = new RestRequest("/tokenlifetime", Method.PUT);
-            request.AddParameter("token", token);
-            request.AddParameter("time", "700000");
+            //login as admin
+            request = new RestRequest("/login", Method.POST);
+            request.AddParameter("name", "admin");          
+            request.AddParameter("password", "qwerty");
             response = client.Execute(request);
-            //string content = response.Content;
-            //Console.WriteLine("Update Tokenlifetime content: " + content);
+            var obj = deserial.Deserialize<RestResult>(response);
+            string token = obj.content;
+            Console.WriteLine("TOKEN =" + token);
+            Assert.IsTrue(token.Length == 32);
             //
+            //create new user
+            request = new RestRequest("/user", Method.POST);
+            request.AddParameter("token", token);
+            request.AddParameter("name", "dima");
+            request.AddParameter("password", "dima1");
+            response = client.Execute(request);
+            obj = deserial.Deserialize<RestResult>(response);
+            Console.WriteLine(obj);
+            Assert.AreEqual("true", obj.content.ToLower());
+            //
+            // getAllUsers
+            request = new RestRequest("/users", Method.GET);
+            request.AddParameter("token", token);
+            response = client.Execute(request);
+            obj = deserial.Deserialize<RestResult>(response);
+            Console.WriteLine("ALL Users: " + obj.content);
+            Assert.IsTrue(obj.content.Contains("dima"));
+            //
+            //log out admin
+            request = new RestRequest("/logout", Method.POST);
+            request.AddParameter("name", "admin");
+            request.AddParameter("token", token);
+            response = client.Execute(request);
+            obj = deserial.Deserialize<RestResult>(response);
+            Console.WriteLine("Logout = " +obj );
+            Assert.AreEqual("true", obj.content.ToLower());
+            //
+            //  Login as dima
+            request = new RestRequest("/login", Method.POST);
+            request.AddParameter("name", "dima");
+            request.AddParameter("password", "dima1");
+            response = client.Execute(request);
+            obj = deserial.Deserialize<RestResult>(response);
+            token = obj.content;
+            Console.WriteLine("Login as dima = "+token);
+            Assert.IsTrue(token.Length == 32);
+            //
+            //change password
+            request = new RestRequest("/user", Method.PUT);
+            request.AddParameter("token", token);
+            request.AddParameter("oldpassword", "dima1");
+            request.AddParameter("newpassword", "dima12");
+            response = client.Execute(request);
+            obj = deserial.Deserialize<RestResult>(response);
+            Console.WriteLine("Change password = " + obj.content);
+            Assert.IsTrue(obj.content.ToLower().Contains("true"));
+            //
+            // logout dima
+            request = new RestRequest("/logout", Method.POST);
+            request.AddParameter("name", "dima");          
+            request.AddParameter("token", token);
+            response = client.Execute(request);
             obj = deserial.Deserialize<RestResult>(response);
             Assert.AreEqual("true", obj.content.ToLower());
             //
-            // get cooldowntime
-            request = new RestRequest("/cooldowntime", Method.GET);
+            //Login with new password
+            request = new RestRequest("/login", Method.POST);
+            request.AddParameter("name", "dima");
+            request.AddParameter("password", "dima12");
             response = client.Execute(request);
-            string content = response.Content;
-            Console.WriteLine("Get Cooldowntime content: " + content);
+            obj = deserial.Deserialize<RestResult>(response);
+            token = obj.content;
+            Console.WriteLine("Token After changing password = " + token);
+            Assert.IsTrue(token.Length == 32);
             //
-            // Change cooldowntime
-            request = new RestRequest("/cooldowntime", Method.PUT);
+            // logout dima
+            request = new RestRequest("/logout", Method.POST);
+            request.AddParameter("name", "dima");          
             request.AddParameter("token", token);
-            request.AddParameter("time", "1100000");
             response = client.Execute(request);
-            content = response.Content;
-            Console.WriteLine("Update Cooldowntime content: " + content);
+            obj = deserial.Deserialize<RestResult>(response);
+            Assert.AreEqual("true", obj.content.ToLower());
             //
-            // get cooldowntime
-            request = new RestRequest("/cooldowntime", Method.GET);
+            //  Login as admin
+            request = new RestRequest("/login", Method.POST);
+            request.AddParameter("name", "admin");          
+            request.AddParameter("password", "qwerty");
             response = client.Execute(request);
-            content = response.Content;
-            Console.WriteLine("Get Cooldowntime content: " + content);
+            obj = deserial.Deserialize<RestResult>(response);
+            token = obj.content;
+            Assert.IsTrue(token.Length == 32);
             //
-            //request = new RestRequest("/tokenlifetime", Method.GET);
-            //response = client.Execute(request);
-            //obj = deserial.Deserialize<RestResult>(response);
-            //Assert.AreEqual("700000", obj.content.ToLower());
+            //remove user
+            request = new RestRequest("/user", Method.DELETE);
+            request.AddParameter("token", token);
+            request.AddParameter("name", "dima");
+            response = client.Execute(request);
+            obj = deserial.Deserialize<RestResult>(response);
+            Console.WriteLine(obj.content.ToLower());
+            Assert.AreEqual("true", obj.content.ToLower());
             //
-            // addItem
-            //request = new RestRequest("/item/{index}", Method.POST);
-            //request.AddParameter("item", "hahaha");          // Save parameters to body
-            //request.AddParameter("token", token);
-            //request.AddUrlSegment("index", "1");
-            //response = client.Execute(request);
-            //obj = deserial.Deserialize<RestResult>(response);
-            //Console.WriteLine("***addItem " + obj);
+            // getAllUsers
+            request = new RestRequest("/users", Method.GET);
+            request.AddParameter("token", token);
+            response = client.Execute(request);
+            obj = deserial.Deserialize<RestResult>(response);
+            Console.WriteLine("ALL Users2: " + obj.content);
+            Assert.IsTrue(!obj.content.Contains("dima"));               
             //
-            // getUserItem
-            //request = new RestRequest("/item/{index}/user/{name}?token=" + token, Method.GET);
-            //request.AddUrlSegment("index", "1");
-            //request.AddUrlSegment("name", "admin");
-            //response = client.Execute(request);
-            //obj = deserial.Deserialize<RestResult>(response);
-            //Console.WriteLine("***getUserItem " + obj);
-            //
-            //request = new RestRequest("/logout", Method.POST);
-            //request.AddParameter("name", "admin");          // Save parameters to body
-            //request.AddParameter("token", token);
-            //response = client.Execute(request);
-            //obj = deserial.Deserialize<RestResult>(response);
-            //Assert.AreEqual("true", obj.content.ToLower());
+            
         }
     }
 }
