9c19a6d8412bdf318557a0c73cb26c88c5891e2f
DimaDomashovets <ddomashovets@gmail.com>
2019-08-24 12:04:43 +0300

    Add logs and fixed tests

--- Rest414Test/Rest414Test/Tests/CreateUser.cs
+++ Rest414Test/Rest414Test/Tests/CreateUser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 using NUnit.Framework;
 using Rest414Test.Data;
 using Rest414Test.Services;
@@ -12,6 +13,8 @@ namespace Rest414Test.Tests
     [TestFixture]
     class CreateUser
     {
+        Logger logger = LogManager.GetCurrentClassLogger();
+        //
         private GuestService guestService = new GuestService();
         private AdminService adminService;
 
@@ -28,6 +31,7 @@ namespace Rest414Test.Tests
         public void CreateNewUser()
         {
             adminService = guestService.SuccessfulAdminLogin(adminUser);
+            logger.Info("Start test CheckLoginUser ");
             Assert.IsTrue(adminService.IsLoggined());
             //
             adminService.CreateUser(simpleUser);
