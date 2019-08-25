e11a22267eeacc66f6d4efc867e854481f0454b9
Nazarii Yurkiv <nazar-y96@ukr.net>
2019-08-25 14:05:31 +0300

    ItemTests create, Add methods to tests

---
+++ Rest414Test/Rest414Test/Tests/ItemsTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Rest414Test.Data;
+using Rest414Test.Services;
+using System.Collections.Generic;
+
+namespace Rest414Test.Tests
+{
+    [TestFixture]
+    public class ItemTest
+    {
+        IUser adminUser = UserRepository.Get().Admin();
+        GuestService guestService = new GuestService();
+        List<ItemTemplate> existItems = ItemsRepository.ListItems();
+        AdminService adminService;
+
+        UserService userService;
+        IUser user = UserRepository.Get().ExistUser();
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+        [Test]
+        public void ItemTestOne()
+        {
+            adminService = guestService.SuccessfulAdminLogin(adminUser);
+            adminService.AddItems(existItems);
+            Assert.AreEqual(existItems.Count, adminService.GetAllItems().Count);
+        }
+        [Test]
+        public void ItemTestTwo()
+        {
+            userService = guestService.SuccessfulUserLogin(user);
+            Assert.IsTrue(userService.IsLoggined());
+            Assert.IsEmpty(userService.GetAllItems());
+        }
+
+    }
+}
